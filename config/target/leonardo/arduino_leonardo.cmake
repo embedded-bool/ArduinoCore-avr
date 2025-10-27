da5093e069b0dbf47b7281750b66a2f235da9ce5
Robert Morley <robjmorley@gmail.com>
2025-10-26 23:52:54 -0400

    Added USB_VID and USB_PID defines
    + added USB_VID and USB_PID defines for the board variants MICRO and LEONARDO

--- config/target/leonardo/arduino_leonardo.cmake
+++ config/target/leonardo/arduino_leonardo.cmake
@@ -4,6 +4,8 @@ if(${BOARD_VARIANT} STREQUAL "LEONARDO")
             "ARDUINO=10810"
             "ARDUINO_AVR_LEONARDO"
             "ARDUINO_ARCH_AVR"
+            "USB_VID=0x2341"
+            "USB_PID=0x8036"
             CACHE INTERNAL "Arduino Leonardo compile defines"
     )
 
--- config/target/micro/arduino_micro.cmake
+++ config/target/micro/arduino_micro.cmake
@@ -4,6 +4,8 @@ if(${BOARD_VARIANT} STREQUAL "MICRO")
             "ARDUINO=10810"
             "ARDUINO_AVR_MICRO"
             "ARDUINO_ARCH_AVR"
+            "USB_VID=0x2341"
+            "USB_PID=0x8037"
             CACHE INTERNAL "Arduino Micro compile defines"
     )
 
